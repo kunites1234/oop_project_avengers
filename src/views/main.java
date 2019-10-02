a33293f9982699543773b1fa7752de2fb8d7b421
kunites1234 <55587686+kunites1234@users.noreply.github.com>
2019-10-02 14:24:21 +0300

    added .gitignore file

---
+++ src/views/main.java
@@ -0,0 +1,16 @@
+package views;
+
+public class main {
+
+    public static void main(String[] args) {
+        LoginView login =new LoginView();
+
+//       names:
+//    	   CUBAKA MUSHAMALIRWA CHRISTOPHE 18/973/BIT-S
+//    	   JOSUE PAUL LUSAKULIRA 17/443/BSCS-S
+//    	   LETEKIDAN TESFAY BELAY 17/697/BSCS-S
+//    	   TINAH KAJ MBANGU 18/896/BSCS-S
+//    	   YOGESH LAKHANI 18/038/BSCS-M
+    }
+
+}
